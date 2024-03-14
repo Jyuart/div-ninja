e615953dddc9e60843bb6063c1374c35332fc4a2
Yurii Palaida <yura.palayda@gmail.com>
2024-03-14 15:19:17 +0000

    update go version; start ws server

--- backend/go.mod
+++ backend/go.mod
@@ -1,8 +1,9 @@
 module jyuart/divninja/backend
 
-go 1.21.6
+go 1.22.1
 
 require (
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	golang.org/x/net v0.17.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
 	"io"
+	"log"
 	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
+// Client manager
+type ClientManager struct {
+	clients sync.Map
+}
+
+func (clientManager *ClientManager) AddClient(clientId string, conn *websocket.Conn) {
+	clientManager.clients.Store(clientId, conn)
+}
+
+func (clientManager *ClientManager) GetClient(clientId string) *websocket.Conn {
+	client, ok := clientManager.clients.Load(clientId)
+	if ok {
+		return client.(*websocket.Conn)
+	}
+	return nil
+}
+
+// Global variables
 var upgrader = websocket.Upgrader{}
+var clientManager ClientManager = ClientManager{}
 
 func main() {
 	http.HandleFunc("/", sayHello)
-	http.HandleFunc("/ws", registerClient)
+	http.HandleFunc("/ws/{id}", registerClient)
 
 	err := http.ListenAndServe(":42069", nil)
 	if err != nil {
-		panic("Something bad happened")
+		handleError(err)
 	}
 }
 
@@ -23,8 +45,44 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func registerClient(w http.ResponseWriter, r *http.Request) {
-	_, err := upgrader.Upgrade(w, r, nil)
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
+		handleError(err)
 		return
 	}
+	connId := r.PathValue("id")
+	clientManager.AddClient(connId, conn)
+	defer conn.Close()
+
+	desktopClientId := r.URL.Query().Get("desktopClientId")
+
+	if desktopClientId == "" {
+		handleDesktopClient(conn)
+	}
+
+	desktopClient := clientManager.GetClient(desktopClientId)
+	if desktopClient == nil {
+		log.Println("Please login the desktop client first")
+		return
+	}
+	handleMobileClient(conn, desktopClient)
+}
+
+func handleDesktopClient(conn *websocket.Conn) {
+	// noting for now
+}
+
+func handleMobileClient(conn *websocket.Conn, desktopConn *websocket.Conn) {
+	// Infinite loop for reading messages
+	for {
+		mt, messageBytes, err := conn.ReadMessage()
+		if err != nil {
+			handleError(err)
+		}
+		message := string(messageBytes)
+	}
+}
+
+func handleError(error error) {
+	log.Println(error)
 }
